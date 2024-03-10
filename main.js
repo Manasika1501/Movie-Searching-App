0a16855c53024334a629724bcad03dcac7719438
Manasi Kashyapi <160340663+Manasika1501@users.noreply.github.com>
2024-03-10 13:33:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form')
+const gallery = document.querySelector('.image-container');
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let query=form.querySelector('input').value;
+    form.querySelector('input').value='';
+    
+    if(query==''){
+        query="nothing";
+    }
+    tvMazeApi(query);
+})
+
+async function tvMazeApi(query){
+    const res=await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
+    const shows=await res.json();
+    
+    makeImages(shows);
+}
+
+function makeImages(shows) {
+    for(let show of shows)
+    {
+          
+            
+            const img = document.createElement('img');
+            img.src=show.show.image.medium;
+            gallery.append(img);
+        
+    }
+}
